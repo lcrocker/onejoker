68b9c1f41529d27632088539e5e2ac44976fab26
Lee Daniel Crocker <lee@piclab.com>
2013-03-09 15:33:10 -0800

    Broken commit, do not build.

--- src/lib/onejoker.c
+++ src/lib/onejoker.c
@@ -32,3 +32,57 @@ int oj_init_library(int seed) {
     _oj_johnnymoss = 0x10ACE0FF;
     return 0;
 }
+
+/* Dealing with card sequences.
+ */
+
+int oj_seq_new(oj_seq_t *sp, int size, int *buf) {
+}
+
+int oj_seq_fill(oj_decktype_t dt) {
+}
+
+int oj_seq_shuffle(oj_sequence_t *sp) {
+}
+
+int oj_seq_deal_from(oj_sequence_t *sp) {
+}
+
+int oj_seq_deal_to(oj_sequence_t *sp, int card) {
+}
+
+int oj_seq_copy(oj_sequence_t *destp, oj_sequence_t *srcp, int count) {
+}
+
+/* Dealing with card names. Should probably do some i18n eventually.
+ */
+
+int oj_card_from_name(char *name, char *next) {
+}
+
+char *oj_name_of_card(int card, oj_cardstyle_t style) {
+}
+
+int oj_seq_name(oj_seq_t *sp, char *name, int size) {
+}
+
+int oj_add_by_name(oj_seq_t *sp, char *names) {
+}
+
+/* Various combinatorics.
+ */
+
+long oj_binomial(int n, int m) {
+}
+
+long oj_iter_new(oj_iterator_t *iter, int length, oj_sequence_t *deck) {
+}
+
+oj_iterator_t *oj_iter_next(oj_iterator_t *iter) {
+}
+
+long oj_colex_rank(oj_sequence_t *hand, oj_sequence_t *deck) {
+}
+
+long oj_hand_at_colex_rank(long r, int length, oj_sequence_t *deck) {
+}
